72d23506786f04cabbd05a3533bef384fc9fec48
KeyboardFire <andy@keyboardfire.com>
2014-07-17 03:35:34 -0500

    Create Player.cpp

---
+++ src/Player.cpp
@@ -0,0 +1,57 @@
+#include "Player.hpp"
+
+Player::Player(std::string cmd): score(0), cmd(cmd) {
+	// nothing to do here
+}
+
+CoordsLine Player::getMove(std::string input) {
+	std::string ERR_STR = "Error in submission \"" + cmd + "\": ";
+	CoordsLine ERR = {{-1, -1}, {-1, -1}};
+
+	FILE* pipe = popen((cmd + " " + input).c_str(), "r");
+	if (!pipe) {
+		std::cerr << ERR_STR << "command not found" << std::endl;
+		return ERR;
+	}
+
+	char buffer[128];
+	std::string result = "";
+	if (fgets(buffer, 128, pipe) != NULL) {
+		result = buffer;
+		fclose(pipe);
+	} else {
+		if (feof(pipe)) {
+			std::cerr << ERR_STR << "no output received" << std::endl;
+		} else if (ferror(pipe)) {
+			std::cerr << ERR_STR << "read error" << std::endl;
+		} else {
+			std::cerr << ERR_STR << "something very weird happened (I/O error)" << std::endl;
+		}
+		fclose(pipe);
+		return ERR;
+	}
+
+	std::istringstream iss(result);
+	int outputCoords[4];
+	for (int i = 0; i < 4; ++i) {
+		iss >> outputCoords[i];
+		if (iss.eof()) {
+			std::cerr << ERR_STR << "not enough input (" << i << " of 4): " << result << std::endl;
+			return ERR;
+		} else if (iss.bad()) {
+			std::cerr << ERR_STR << "error in istringstream (something weird happened): " << result << std::endl;
+			return ERR;
+		} else if (iss.fail()) {
+			std::cerr << ERR_STR << "parse error: " << result << std::endl;
+			return ERR;
+		}
+	}
+
+	return CoordsLine{{outputCoords[0], outputCoords[1]}, {outputCoords[2], outputCoords[3]}};
+}
+
+std::string Player::str() {
+	std::ostringstream oss;
+	oss << cmd << " " << score;
+	return oss.str();
+}
